cdad095a5992c091832180d8413ec2adc1bce431
Shavkatjon97 <dadshavkatjon97@gmail.com>
2022-08-03 17:14:31 +0500

    Task in Module was done

--- src/main/java/lang/print/gaps/task1/UpdatedApp.java
+++ src/main/java/lang/print/gaps/task1/UpdatedApp.java
@@ -5,6 +5,6 @@ import java.util.Scanner;
 public class UpdatedApp {
     public static void main(String[] args) {
 
-        System.out.println("I'm updated \n"+"can be compiled. And can write to console \n");
+        System.out.println("I'm updated \n"+"can be compiled. And can write to console");
     }
 }
